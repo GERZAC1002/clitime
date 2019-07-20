08436e4680a9e47b55d79dd4b76f1b56c9bf0c60
GERZAC1002 <gernot.zacharias@gmx.de>
2019-07-20 20:56:36 +0200

    Data upload

---
+++ clizeit.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "zeigzahl.h"
+
+char ersatz = 'o';
+
+int main(){
+	//setvbuf(stdout, (char*)NULL, _IONBF, 0);
+	while(1){
+		long int zeit = 0;
+		zeit = time(NULL);
+		zeit = zeit + (60*60*2);
+		int stunde,minute = 0;
+		//int jahr,monat,tag = 0;
+		minute = zeit%3600/60;// sekunden modulo ( sekunden * minuten ) div ( sekunden )
+		stunde = zeit%86400/3600;// sekunden modulo ( sekunden * minuten * stunden ) div ( sekunden * minuten )
+		//tag = zeit%31536000/86400;
+		//printf("%d:%d\n",stunde,minute);
+		int s1,s2,m1,m2 = 0;
+		s1 = stunde%10;
+		s2 = stunde/10;
+		m1 = minute%10;
+		m2 = minute/10;
+		for(int i = 0; i < 9; i++){
+			printf("\x1B[36m");
+			zeige_zahl(s2,i,ersatz);
+			zeige_zahl(s1,i,ersatz);
+			zeige_zahl(99,i,ersatz);
+			zeige_zahl(m2,i,ersatz);
+			zeige_zahl(m1,i,ersatz);
+			printf("\n");
+		}
+		//printf("s1:%ds2:%dm1:%dm2:%d",s1,s2,m1,m2);
+		sleep(1);
+		system("clear");
+	}
+}
---
+++ zeigzahl.h
@@ -0,0 +1,50 @@
+#include "numbers.h"
+
+int anzeige_zahl(char text[9][5],int durch,char ersatz){
+	for(int i = 0; i < 5 ; i++){
+		if(text[durch][i] == '#'){
+			printf("%c ",ersatz);
+		}else{
+			printf("%c ",' ');
+		}
+	}
+	printf(" ");
+	return 0;
+}
+
+int zeige_zahl(int i,int durch,char ersatz){
+	if(i == 0){
+		anzeige_zahl(nul,durch,ersatz);
+	}
+	if(i == 1){
+		anzeige_zahl(eins,durch,ersatz);
+	}
+	if(i == 2){
+		anzeige_zahl(zwei,durch,ersatz);
+	}
+	if(i == 3){
+		anzeige_zahl(drei,durch,ersatz);
+	}
+	if(i == 4){
+		anzeige_zahl(vier,durch,ersatz);
+	}
+	if(i == 5){
+		anzeige_zahl(funf,durch,ersatz);
+	}
+	if(i == 6){
+		anzeige_zahl(sechs,durch,ersatz);
+	}
+	if(i == 7){
+		anzeige_zahl(sieben,durch,ersatz);
+	}
+	if(i == 8){
+		anzeige_zahl(acht,durch,ersatz);
+	}
+	if(i == 9){
+		anzeige_zahl(neun,durch,ersatz);
+	}
+	if(i == 99){
+		anzeige_zahl(dp,durch,ersatz);
+	}
+	return 0;
+}
